b79427914f4f028bc8fb709aec2d54a560274868
yasmina678 <112327938+yasmina678@users.noreply.github.com>
2023-12-08 22:24:57 +0200

    Add files via upload

---
+++ codealpha_task4/js/main.js
@@ -0,0 +1,33 @@
+
+let allMusic = [
+    {
+      name: "Still With You",
+      artist: "Jung Kook",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Stay With Me",
+      artist: "CHANYOL, punch",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Promise",
+      artist: "Jimin",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Slow Dancing",
+      artist: "V",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Some",
+      artist: "BOL4",
+      img: "music-5",
+      src: "music-5"
+    },
+  ];
